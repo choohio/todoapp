4d5069ac9cc13c994df7d108d8949e3deaa9f1e7
chookha <alchukharev@mail.ru>
2023-09-14 23:40:27 +0300

    feat: logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,28 @@
+'use client';
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+
 export default function ProfilePage() {
+    const router = useRouter();
+
+    const logout = async () => {
+        try {
+            await axios.get('/api/users/logout')
+            router.push('/login')
+
+        } catch (error: any) {
+            console.log(error.message)
+        }
+
+    }
     return (
         <div className="flex flex-col">
             <h1>Profile</h1>
             <hr/>
             <p>Profile page</p>
+            <button onClick={logout}>Log Out</button>
 
         </div>
     )
